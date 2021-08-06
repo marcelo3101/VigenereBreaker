412f600ccdeae619a59f441805a2183a2e97f4f3
map3101 <marcelo.postiglione@gmail.com>
2021-08-06 17:48:20 -0300

    Adiciona decifrador e corrige bugs na implementação das funções

--- t1_vigenere.c
+++ t1_vigenere.c
@@ -1,40 +1,63 @@
 /*
     Trabalho 1 de segurança computacional
 */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
-// Cifrador
-void cifrador(char keyword[20], char message[20]) {
-    char extkeyword[20], encripted[20];
-    // Montar Keystream
+// Gerador de Keystream
+char * keystream_generator(char secret_word[20], char message[20]) {
+    char * extkeyword = malloc(sizeof(char)*20);
     for(int i = 0, j = 0; i < strlen(message); i++, j++) {
-        if(j == strlen(keyword)) j = 0;
-
-        extkeyword[i] = keyword[j];
-
+        if(j == strlen(secret_word)) j = 0;
+        extkeyword[i] = secret_word[j];
+        // Adiciona null byte
         if(i == strlen(message)-1) extkeyword[i+1] = '\0';
     }
 
-    printf("%s\n", extkeyword);
+    return extkeyword;
+}
+
+// Cifrador
+char * cifrador(char keystream[20], char message[20]) {
+    char * encripted = malloc(sizeof(char)*20);
 
     for( int i = 0; i < strlen(message); i++) {
-        char x = ((message[i] + extkeyword[i]) % 26) + 'A';
-        encripted[i] = x;
+        encripted[i] = ((message[i] + keystream[i]) % 26) + 'A';
     }
     
-    printf("%s\n", encripted);
+    return encripted;
+}
+
+// Decifrador
+char * decifrador(char keystream[20], char encripted_message[20]){
+    char * decripted = malloc(sizeof(char)*20);
+
+    for( int i = 0; i < strlen(encripted_message); i++) {
+        decripted[i] = ((encripted_message[i] - keystream[i] + 26) % 26) + 'A';
+    }
+    
+    return decripted;
 }
 
 int main(){
+    char message[20], keyword[20], * keystream = malloc(sizeof(char) * 20), * encriptedmessage = malloc(sizeof(char) * 20), * decriptedmessage = malloc(sizeof(char) * 20);
     // Receber keyword e mensagem
-    char message[20], keyword[20];
     printf("Digite a keyword: ");
     scanf("%s", &keyword);
     printf("Digite a mensagem: ");
     scanf("%s", &message);
-    cifrador(keyword, message);
+    
+    // Montar a Keystream
+    keystream = keystream_generator(keyword, message);
+
+    // Encriptar e decriptar
+    encriptedmessage = cifrador(keystream, message);
+    decriptedmessage = decifrador(keystream, encriptedmessage);
+    printf("%s\n", encriptedmessage);
+    printf("%s\n", decriptedmessage);
 
     return 0;
 }
